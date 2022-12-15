b473c0e019661673e78a9e024aaff6fde0b139d2
Maria Riosdelgado <mariariosdelgado@Marias-iMac.local>
2022-12-15 07:55:23 -0500

    added frontend

---
+++ src/components/loginform.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./loginform.css"
+
+const LoginForm = () => {
+    return (
+        <div className="cover">
+            <h1>Login</h1>
+            <input type="text" placeholder="username" />
+            <input type="password" placeholder="password" />
+
+        </div>
+    )
+}
+
+export default LoginForm
